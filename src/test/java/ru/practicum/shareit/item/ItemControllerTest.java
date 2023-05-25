dde9109c31c495b7cb10700ede886a235c759060
Наумов Максим <dusa11223344@yandex.ru>
2023-05-25 12:12:48 +0300

    Добавлены тесты

---
+++ src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -0,0 +1,151 @@
+package ru.practicum.shareit.item;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import ru.practicum.shareit.exceptions.DataNotFoundException;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.UserController;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static ru.practicum.shareit.item.mapper.ItemMapper.toItemDto;
+
+@SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class ItemControllerTest {
+    private final ItemController controller;
+    private final UserController userController;
+    private ItemDto item;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+
+        item = ItemDto.builder()
+                .name("Дрель")
+                .description("Сверлит")
+                .available(true)
+                .build();
+
+        user = User.builder()
+                .name("Макс")
+                .email("email@email.ru")
+                .build();
+        userController.create(user);
+        controller.create(1L, item);
+    }
+
+    @Test
+    void findAll() {
+        item.setId(1L);
+
+        assertEquals(List.of(item), controller.findAll(1L));
+    }
+
+    @Test
+    void findById() {
+        item.setId(1L);
+
+        assertEquals(item, controller.findById(1L));
+    }
+
+    @Test
+    void findByIdWrongId() {
+        item.setId(1L);
+        final DataNotFoundException exception = assertThrows(DataNotFoundException.class,
+                () -> controller.findById(2L));
+
+        assertEquals("Предмет с id 2 не найден", exception.getParameter());
+    }
+
+    @Test
+    void create() {
+        Item newItem = Item.builder()
+                .id(2L)
+                .name("Бензопила")
+                .description("Пилит")
+                .available(true)
+                .build();
+
+        assertEquals(toItemDto(newItem), controller.create(1L, toItemDto(newItem)));
+    }
+
+    @Test
+    void update() {
+        controller.create(1L, item);
+        Item updatedItem = Item.builder()
+                .id(1L)
+                .name("Бензопила")
+                .description("Пилит")
+                .available(true)
+                .build();
+        controller.update(toItemDto(updatedItem), 1L, user.getId());
+
+        assertEquals(toItemDto(updatedItem), controller.findById(1L));
+    }
+
+    @Test
+    void updateWithWrongUser() {
+        Item updatedItem = Item.builder()
+                .id(1L)
+                .name("Бензопила")
+                .description("Пилит")
+                .available(true)
+                .build();
+        User user2 = User.builder()
+                .name("Макс")
+                .email("d@g.ru")
+                .build();
+        userController.create(user2);
+        final DataNotFoundException exception = assertThrows(DataNotFoundException.class, () ->
+                controller.update(toItemDto(updatedItem), 1L, 2L));
+
+        assertEquals("Этот предмет не принадлежит пользователю с id 2", exception.getParameter());
+    }
+
+    @Test
+    void search() {
+        Item itemToSearch = Item.builder()
+                .id(2L)
+                .name("Бензопила")
+                .description("Пилит")
+                .available(true)
+                .build();
+        User user2 = User.builder()
+                .name("Макс")
+                .email("d@g.ru")
+                .build();
+        userController.create(user2);
+        controller.create(user2.getId(), toItemDto(itemToSearch));
+
+        assertEquals(List.of(toItemDto(itemToSearch)), controller.search(user.getId(), "Бензопила"));
+    }
+
+    @Test
+    void searchWithWrongData() {
+        Item itemToSearch = Item.builder()
+                .id(2L)
+                .name("Бензопила")
+                .description("Пилит")
+                .available(true)
+                .build();
+        User user2 = User.builder()
+                .name("Макс")
+                .email("d@g.ru")
+                .build();
+        userController.create(user2);
+        controller.create(user2.getId(), toItemDto(itemToSearch));
+
+        assertEquals(Collections.emptyList(), controller.search(user.getId(), ""));
+    }
+}
---
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,94 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import ru.practicum.shareit.exceptions.DataNotFoundException;
+import ru.practicum.shareit.exceptions.NotUniqException;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ru.practicum.shareit.user.mapper.UserMapper.toUserDto;
+
+@SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class UserControllerTest {
+    private final UserController controller;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .name("Макс")
+                .email("email@email.ru")
+                .build();
+        controller.create(user);
+    }
+
+    @Test
+    void getAll() {
+        assertEquals(List.of(toUserDto(user)), controller.getAll());
+    }
+
+    @Test
+    void getById() {
+        assertEquals(toUserDto(user), controller.getById(1L));
+    }
+
+    @Test
+    void getByIdWrongId() {
+        final DataNotFoundException exception = assertThrows(DataNotFoundException.class,
+                () -> controller.getById(2L));
+
+        assertEquals("Пользователь с id 2 не найден", exception.getParameter());
+    }
+
+    @Test
+    void create() {
+        User userForCreate = User.builder()
+                .name("Max")
+                .email("e@mail.ru")
+                .build();
+        controller.create(userForCreate);
+
+        assertEquals(toUserDto(userForCreate), controller.getById(2L));
+    }
+
+    @Test
+    void createWithNotUniqueEmail() {
+        User userForUpdate = User.builder()
+                .name("Max")
+                .email("email@email.ru")
+                .build();
+        final NotUniqException exception = assertThrows(NotUniqException.class, () ->
+                controller.create(userForUpdate));
+
+        assertEquals("Пользователь с таким email уже зарегестрирован", exception.getParameter());
+    }
+
+    @Test
+    void update() {
+        User userForUpdate = User.builder()
+                .id(1L)
+                .name("Max")
+                .email("e@mail.ru")
+                .build();
+        controller.update(userForUpdate, 1L);
+
+        assertEquals(toUserDto(userForUpdate), controller.getById(1L));
+    }
+
+    @Test
+    void delete() {
+        assertEquals(List.of(toUserDto(user)), controller.getAll());
+        controller.delete(1L);
+        assertEquals(Collections.emptyList(), controller.getAll());
+    }
+}
